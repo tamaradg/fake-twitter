ea079685a93c8a80c8d858a56e97c28de2888062
Sébastien Saunier <seb@saunier.me>
2017-08-08 17:34:25 +0100

    Fake twitter is now ready

--- src/fake_twitter.js
+++ src/fake_twitter.js
@@ -1,1 +1,12 @@
-console.log("Hello from src/browser.js");
+function loadNewTweet() {
+  // Your turn!
+  console.log("loadNewTweet called");
+
+}
+
+
+
+
+
+// Call loadNewTweet every 3s;
+setInterval(loadNewTweet, 3000);
